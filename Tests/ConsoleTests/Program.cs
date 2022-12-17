ae28d54faeeaa9fdb44b90dffa655a60f9c1b9d1
Егор Люханов <ealuhanov@yandex.ru>
2022-12-17 23:03:11 +0500

    Prepare process for testing (notepad)

--- Tests/ConsoleTests/Program.cs
+++ Tests/ConsoleTests/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ConsoleTests
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleTests
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var notepad = Process.Start("notepad");
+
+
+            Console.WriteLine("End.");
+            Console.ReadLine();
+
+            notepad.CloseMainWindow();
         }
     }
 }
